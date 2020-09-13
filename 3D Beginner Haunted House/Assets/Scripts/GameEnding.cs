c33754217fa6ef07cef84c2eabd27ae0a81f61f1
Angeline Siv <siv.angeline@ymail.com>
2020-09-12 23:53:06 -0400

    Add game end event

---
+++ 3D Beginner Haunted House/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using System.Threading;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerAtExit = false;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    float m_Timer;
+
+    void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
