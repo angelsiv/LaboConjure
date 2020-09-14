8c0cc51f472b1c473f55bb9ed103628242d216c1
Angeline <siv.angeline@ymail.com>
2020-09-13 23:40:26 -0400

    Add Gargoyle enemy
    Set idle animation
    Add "catch player" behavior
    Create prefab

--- 3D Beginner Haunted House/Assets/Scripts/GameEnding.cs
+++ 3D Beginner Haunted House/Assets/Scripts/GameEnding.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using System.Runtime.Hosting;
 using System.Threading;
 using UnityEngine;
@@ -10,16 +11,23 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
+
     bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught = false;
 
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     float m_Timer;
 
     void Update()
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
@@ -31,14 +39,28 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-    void EndLevel()
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
         
         if(m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
+
+        
     }
 }
