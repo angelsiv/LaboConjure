093bea885e345f768a8f8e30cf6fd687f3d006f0
Angeline <siv.angeline@ymail.com>
2020-09-20 19:28:39 -0400

    Update main menu scene
    Add ghost moving in background
    Add buttons to start game and exit

---
+++ 3D Beginner Haunted House/Assets/MenuOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuOptions : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
