707fa5a41b56964b9be8bd873435152350e58893
Angeline <siv.angeline@ymail.com>
2020-09-21 22:10:43 -0400

    Add audiomixer

---
+++ 3D Beginner Haunted House/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer m_audioMixer;
+    [SerializeField] Canvas m_OptionsCanvas;
+
+    private AudioMixerGroup m_Master;
+    private AudioMixerGroup m_Music;
+    private AudioMixerGroup m_Sfx;
+
+
+}
