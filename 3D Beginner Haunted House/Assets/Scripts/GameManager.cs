c2e996311b885aaff7aa02d6f0a030d5b0fff9b2
Angeline <siv.angeline@ymail.com>
2020-09-26 21:00:21 -0400

    Add options canvas

--- 3D Beginner Haunted House/Assets/Scripts/GameManager.cs
+++ 3D Beginner Haunted House/Assets/Scripts/GameManager.cs
@@ -5,12 +5,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] AudioMixer m_audioMixer;
-    [SerializeField] Canvas m_OptionsCanvas;
-
-    private AudioMixerGroup m_Master;
-    private AudioMixerGroup m_Music;
-    private AudioMixerGroup m_Sfx;
-
 
 }
--- 3D Beginner Haunted House/Assets/Scripts/MenuOptions.cs
+++ 3D Beginner Haunted House/Assets/Scripts/MenuOptions.cs
@@ -1,10 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class MenuOptions : MonoBehaviour
 {
+    [SerializeField] private AudioMixer m_audioMixer;
+    [SerializeField] private Canvas m_OptionsCanvas;
+
+    private GameManager GameManager;
+
+    private AudioMixerGroup m_Master;
+    private AudioMixerGroup m_Music;
+    private AudioMixerGroup m_Sfx;
+
+    private void Start()
+    {
+        GameManager = FindObjectOfType<GameManager>();
+
+        m_OptionsCanvas.gameObject.SetActive(false);
+        m_OptionsCanvas.enabled = false;
+    }
+
+    public void DisplayOptionsCanvas()
+    {
+        if(m_OptionsCanvas != null)
+        {
+            if(!m_OptionsCanvas.isActiveAndEnabled)
+            {
+                m_OptionsCanvas.gameObject.SetActive(true);
+                m_OptionsCanvas.enabled = true;
+            }
+            else
+            {
+                m_OptionsCanvas.gameObject.SetActive(false);
+                m_OptionsCanvas.enabled = false;
+            }
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
